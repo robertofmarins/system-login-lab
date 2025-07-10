14018a672a61f5e462db4af475c79153c6d7c288
robertofmarins <robertofmarins@outlook.com>
2025-07-09 21:05:06 -0300

    Atualização

--- src/app/api/auth/[id]/route.ts
+++ src/app/api/auth/[id]/route.ts
@@ -1,30 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export async function DELETE(request: NextRequest, { params }: Params) {
-  const id = parseInt(params.id, 10);
+export async function DELETE(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = parseInt(context.params.id);
 
   if (isNaN(id)) {
     return NextResponse.json({ error: 'ID inválido' }, { status: 400 });
   }
 
   try {
-    const user = await prisma.user.delete({ where: { id } });
+    const user = await prisma.user.delete({
+      where: { id },
+    });
+
     return NextResponse.json(
       { message: 'Usuário excluído com sucesso', user },
       { status: 200 }
     );
   } catch (error) {
     console.error('Erro ao excluir usuário:', error);
-    return NextResponse.json(
-      { error: 'Erro ao excluir usuário' },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: 'Erro ao excluir usuário' }, { status: 500 });
   }
 }
